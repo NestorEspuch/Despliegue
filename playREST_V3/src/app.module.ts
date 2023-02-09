707ee545342ae5f37ef2c3c288b4fc807226d9be
Nestor Espuch <nestorespuch@gmail.com>
2023-02-09 16:35:59 +0100

    Edit url mongodb

--- playREST_V3/src/app.module.ts
+++ playREST_V3/src/app.module.ts
@@ -14,7 +14,7 @@ import { UsuarioModule } from './usuario/usuario.module';
     MongooseModule.forFeature([{ name: 'Auth', schema: UsuarioSchema }]),
     JuegoModule,
     UsuarioModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/playRest_v3'),
+    MongooseModule.forRoot('mymongodb://my:27017/playRest_v3'),
     MulterModule.register({
       dest: './public/uploads',
     }),
